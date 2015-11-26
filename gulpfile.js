d732c84c7e591fff065af97145bdf6c9e92afda6
Costava <Costava@users.noreply.github.com>
2015-11-26 01:16:32 -0600

    Updated dev files

--- gulpfile.js
+++ gulpfile.js
@@ -8,13 +8,19 @@ var del = require('del');
 var Promise = require('bluebird');
 var webpack = require('webpack');
 
-var webpackConfig = require('./webpack.config.js');
-
-// Default Task
+// Default task, dev build
 gulp.task('default', function(callback) {
 	runSequence(
 		['build-clean'],
 		['move-html', 'move-css', 'build-scripts'],
+		callback
+	);
+});
+
+gulp.task('production-build', function(callback) {
+	runSequence(
+		['build-clean'],
+		['move-html', 'move-css', 'build-scripts-production'],
 		callback
 	);
 });
@@ -34,11 +40,27 @@ gulp.task('move-css', function() {
 		.pipe(gulp.dest('./dist/styles'));
 });
 
-gulp.task('build-scripts', function() {
-  return Promise.promisify(webpack)(webpackConfig)
+function build(config) {
+	return Promise.promisify(webpack)(config)
 	.catch(function(err) {
 		if(err) {
 			throw new gutil.PluginError('webpack', err);
 		}
 	});
+}
+
+gulp.task('build-scripts', function() {
+	var webpackConfig = require('./webpack.config.js');
+
+	build(webpackConfig);
+});
+
+gulp.task('build-scripts-production', function() {
+	var webpackConfig = require('./webpack.config.js');
+	webpackConfig.plugins = [
+		new webpack.optimize.UglifyJsPlugin(),
+		new webpack.optimize.OccurenceOrderPlugin()
+	];
+
+	build(webpackConfig);
 });
