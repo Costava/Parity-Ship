72cfdcae82cc140f7acd2c316f4ccd2bc5bb2d7a
Costava <Costava@users.noreply.github.com>
2015-12-05 01:49:16 -0600

    Cleaned loop code

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -38,9 +38,7 @@ function loop() {
 		var sideLength = Util.randomInInterval(0.3, 1.2) * radius;
 
 		var speed = Util.randomInInterval(game.minShipSpeed, game.maxShipSpeed);
-		speed *= -1;//so that ship goes to left;
-
-		// console.log('ship', x, y);
+		speed *= -1;// so that ship goes to left
 
 		var ship = new Ship({x: x, y: y}, radius, sideLength, Color.random(Util.randomInInterval(0.7, 0.9)), {x: speed, y: 0});
 
@@ -52,7 +50,6 @@ function loop() {
 	// check if should make new planet
 	if (game.newTime - game.newPlanetTime > game.newPlanetInterval) {
 		// make a new planet
-
 		var x = Math.random() * (3+game.maxPlanetLength) + 100;
 		var y = Math.random() * game.maxY();
 
@@ -65,7 +62,7 @@ function loop() {
 		color.b *= mult;
 
 		var speed = 0.010 * sideLength;
-		speed *= -1;// so planet goes to left
+		speed *= -1;// so that planet goes to left
 
 		var planet = new Planet({x: x, y: y}, sideLength, color, {x: speed, y: 0});
 
@@ -82,7 +79,7 @@ function loop() {
 	}
 
 	// move planets
-	game.planets.forEach(function(planet, index, planets) {
+	game.planets.forEach(function(planet) {
 		var dx = planet.speed.x * game.dt;
 		var dy = planet.speed.y * game.dt;
 
@@ -91,7 +88,7 @@ function loop() {
 	});
 
 	// move ships
-	game.ships.forEach(function(ship, index, ships) {
+	game.ships.forEach(function(ship) {
 		var dx = ship.speed.x * game.dt;
 		var dy = ship.speed.y * game.dt;
 
@@ -100,31 +97,31 @@ function loop() {
 	});
 
 	// move clean packets
-	game.cleanPackets.forEach(function(packet, index, packets) {
+	game.cleanPackets.forEach(function(packet) {
 		var dx = packet.speed.x * game.dt;
 
 		packet.pos.x += dx;
 	});
 
 	// remove ships out of bounds
-	game.ships.forEach(function(ship, index, ships) {
+	game.ships.forEach(function(ship) {
 		// if ship is left of screen or (ship is right and screen and has been hit)
 		if (ship.pos.x < 0 || (ship.pos.x > 100 && ship.hits > 0)) {
 			game.ships.splice(game.ships.indexOf(ship), 1);
 		}
 	});
 
-	//remove planets out of bounds
-	game.planets.forEach(function(planet, index, planets) {
+	// remove planets out of bounds
+	game.planets.forEach(function(planet) {
 		if (planet.pos.x < 0){
 			game.planets.splice(game.planets.indexOf(planet), 1);
 		}
 	});
 
 	// check for CleanPacket-ship collisions;
-	game.cleanPackets.forEach(function(packet, packetIndex, packets) {
+	game.cleanPackets.forEach(function(packet) {
 		if (!packet.hit) {
-			game.ships.some(function(ship, shipIndex, ships) {
+			game.ships.some(function(ship) {
 				if (!ship.hit) {
 					var distance = Vector2.distance(packet.pos, ship.pos);
 					var colDistance = packet.radius + ship.radius;
@@ -191,13 +188,12 @@ function loop() {
 	});
 
 	//check for player-ship collisions
-	game.ships.some(function(ship, index, ships) {
+	game.ships.some(function(ship) {
 		var distance = Vector2.distance(game.player.pos, ship.pos);
 		var colDistance = game.player.radius + ship.radius;
 
-		// console.log(distance, colDistance);
 		if (distance < colDistance) {
-			//game over
+			// game over
 			console.log('Game over!');
 			game.looping = false;
 
@@ -207,8 +203,7 @@ function loop() {
 			document.removeEventListener('click', game.ShootListener);
 
 			// stop ship speed step ups that are left
-			game.shipSpeedStepUps.forEach(function(step, index, steps) {
-				// console.log('clearTimeout');
+			game.shipSpeedStepUps.forEach(function(step) {
 				window.clearTimeout(step);
 			});
 
@@ -249,16 +244,15 @@ function loop() {
 				}, (interval * numIntervals + 500) + i * interval);
 			}
 
-			return true;//stop checking ships
+			return true;// stop checking ships
 		}
 	});
 
-	game.colorChangers.forEach(function(cc, index, ccs) {
-		// console.log(cc.target.color.toString());
+	game.colorChangers.forEach(function(cc) {
 		cc.change(game.dt);
 	});
 
-	//invert inner color of player before drawing
+	// invert inner color of player before drawing
 	game.player.innerColor = game.player.color.inverse();
 
 	game.draw();
